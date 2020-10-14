5169e63fb18da6a9d26730665600608d26799501
dos4dev <dos4dev@gmail.com>
2020-10-14 20:00:08 +0300

    Override SwaggerResourcesProvider by RegistrySwaggerResourcesProvider class, which allows receiving documentation of all registered microservices.

---
+++ redirect/src/main/java/com/example/zuul/redirection/config/RegistrySwaggerResourcesProvider.java
@@ -0,0 +1,49 @@
+package com.example.zuul.redirection.config;
+
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Retrieves all registered microservices Swagger resources.
+ */
+@Primary
+@Component
+public class RegistrySwaggerResourcesProvider implements SwaggerResourcesProvider {
+
+    private final RouteLocator routeLocator;
+
+    public RegistrySwaggerResourcesProvider(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+
+
+        resources.add(swaggerResource("eureka", "/v2/api-docs"));
+
+        //Add the registered microservices swagger docs as additional swagger resources
+        List<Route> routes = routeLocator.getRoutes();
+        routes.forEach(route -> {
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs")));
+        });
+
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
+    }
+}
